b4aa1a8e3f49f3b5d7b1d0d2baf99aaace3af5e7
Tihomir Piha <piha.tihomir@gmail.com>
2015-12-23 04:51:46 +0100

    removed test checks json

--- checks_test.go
+++ checks_test.go
@@ -5,19 +5,19 @@ import "testing"
 // TestChecksManager checks the CheckManager object loading and parsing capabilities for checks JSON file
 func TestChecksManager(t *testing.T) {
 	cm := ChecksManager{}
-	err := cm.Load("checks_test.json")
+	err := cm.Load("checks.sample.json")
 
 	if err != nil {
 		t.Errorf("[TestChecksManager] ChecksManger.Load failed: %s", err)
 	}
 
-	chkParsed := cm.Checks["check1"].(map[string]interface{})
+	chkParsed := cm.Checks["check_some_content"].(map[string]interface{})
 
 	if chkParsed["type"] != "file_contains" {
 		t.Errorf("[TestChecksManager] Wrong check type: %s", chkParsed["type"])
 	}
 
-	if chkParsed["path"] != "/some/path/to/file" {
+	if chkParsed["path"] != "/path/to/some/file" {
 		t.Errorf("[TestChecksManager] Wrong check path: %s", chkParsed["path"])
 	}
 
